328217fa0aebb642a3b1f0214a78f4f06f226b95
Raphael Bacher <raphael.bacher@univ-grenoble-alpes.fr>
2018-11-09 01:30:38 +0100

    working buffer line

--- src/lines.jl
+++ src/lines.jl
@@ -1,27 +1,42 @@
 
 function insert_lines!(js_scene, points)
     evaljs(js_scene, @js begin
-        @var scene = window.scene
-        @var THREE = window.THREE
-        console.log(THREE)
-        @var material = @new THREE.LineBasicMaterial(d(
-            color = "#001100"
-        ));
-        @var geometry = @new THREE.Geometry();
         @var points = $(points[]);
-        for i=0:points.length-1
-            # TODO can we directly serialize this as a Vector3?
-            geometry.vertices.push(@new(THREE.Vector3(points[i][0], points[i][1], points[i][2])))
-        end
+        n=points.length
+        @var scene = this.scene
+        @var THREE = this.THREE
+        @var material = @new THREE.LineBasicMaterial(d(
+            color = "#ffffff"
+        ));
+        @var geometry = @new THREE.BufferGeometry();
+        @var pos = @new Float32Array(n*3)
+        geometry.addAttribute("position", @new THREE.BufferAttribute( pos,3 ));
+        geometry.dynamic = true;
         scene.add(@new THREE.Line(geometry, material))
+        positions = geometry.attributes.position.array
+
+
+        for i=0:n-1
+            # TODO can we directly serialize this as a Vector3?
+            #geometry.vertices.push(@new(THREE.Vector3(points[i][0], points[i][1], points[i][2])))
+            positions[i*3] = points[i][0]
+            positions[i*3+1] = points[i][1]
+            positions[i*3+2] = points[i][2]
+            #geometry.addAttribute( "color", @new THREE.Float32BufferAttribute(
+            #                        [points[i][0], points[i][1], points[i][2]], 3));
+
+        end
+        geometry.computeBoundingSphere();
+
+
     end)
 end
 
 
 w = Window()
-js_scene = ThreeScene();
+#js_scene = ThreeScene();
+js_scene = ThreeScreen();
 tools(w)
-points = tuple.(rand(10).* 100, rand(10).* 100, rand(10).* 100)
-body!(w, js_scene(dom"div#container"()))
-points = Observable(js_scene, "points", tuple.(rand(10).* 10, rand(10).* 10, rand(10).* 10))
-insert_lines!(js_scene, points)
+body!(w, js_scene.context(dom"div#container"()))
+points = Observable(js_scene.context, "points", tuple.(rand(10).* 10, rand(10).* 10, rand(10).* 10))
+insert_lines!(js_scene.context, points)
